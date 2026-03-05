69f4e5e9446fa25a956473b8ce15fb074c6a8717
Kavynayashankar07 <kavynayashankar07@gmail.com>
2026-03-05 21:51:02 +0530

    usecase-6

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -2,27 +2,35 @@ import java.util.Stack;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String input="monish";
-        int i,n;
-        Stack<Character> stack=new Stack<>();
-        for(char c:input.toCharArray()){
-            stack.push(c);
+        System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
+        System.out.println("version:6.0");
+        System.out.println("System instanced successful");
+        System.out.println();
+
+        String Palindrome = "racecar";
+        boolean flag=true;
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        for(int i=0;i<Palindrome.length();i++)
+        {
+            stack.push(Palindrome.charAt(i));
+            queue.add(Palindrome.charAt(i));
         }
-        boolean isPalindrome=true;
-        for(char c:input.toCharArray()){
-            char poppedChar=stack.pop();
-            if(c!=poppedChar){
-                isPalindrome=false;
-                break;
+
+        for(int i=0;i<Palindrome.length();i++)
+        {
+            char chs = stack.pop();
+            char chq = queue.remove();
+
+            if(Palindrome.charAt(i)!=chs && Palindrome.charAt(i)!=chq)
+            {
+                flag = false;
             }
         }
-        if(isPalindrome){
-            System.out.println("Input: "+input);
-            System.out.println("Is palindrome? : true");
-        }else{
-            System.out.println("Input: "+input);
-            System.out.println("Is palindrome? : false");
-        }
+
+        if (flag)  System.out.println("The string " + Palindrome + " is a palindrome.");
+        else System.out.println("The string " + Palindrome + " is not a palindrome .");
     }
 }
 
